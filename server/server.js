aa7451165d593064aac9b2d1c4df05dd8317028f
Jordan Larios <jolarios96@gmail.com>
2022-08-15 11:12:28 -0500

    initial server file

---
+++ server/server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require("express")();
+const app = express;
+
+// placeholder data
+app.get("/api", (req, res) => {
+  res.json({ numbers: { one, two, three } });
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () =>
+  console.log(
+    `Running server in ${process.env.NODE_ENV}mode,\nServer started on ${PORT}.`
+  )
+);
